8d3bbfc0331eecb04706a21bc198b0f134d680cd
VYSHNAVI K <170030547@kluniversity.in>
2020-07-26 21:50:56 +0530

    Task-4

---
+++ Task-4/src/main/java/Epam/Task_4/ConstructionCost.java
@@ -0,0 +1,31 @@
+package Epam.Task_4;
+
+
+public class ConstructionCost {
+	public double CalculateConstructionCost(String StandardMaterial,double Housearea)
+	  {
+	 double con_cost=0.0;
+	 if(StandardMaterial=="Standard")
+	 {
+	 int StandardMaterialcost=1200;
+	 con_cost=StandardMaterialcost*Housearea;
+	 }
+	 else if(StandardMaterial=="AboveStandard")
+	 {
+	 int StandardMaterialcost=1500;
+	 con_cost=StandardMaterialcost*Housearea;
+	 }
+	 else if(StandardMaterial=="HighStandard")
+	 {
+	 int StandardMaterialcost=1800;
+	 con_cost=StandardMaterialcost*Housearea;
+	 }
+	 else if(StandardMaterial=="FullyAutomated")
+	 {
+	 int StandardMaterialcost=2500;
+	 con_cost=StandardMaterialcost*Housearea;
+	 }
+	 return con_cost;
+	  }
+}
+
---
+++ Task-4/src/main/java/Epam/Task_4/ConstructionCostTest.java
@@ -0,0 +1,13 @@
+package Epam.Task_4;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+public class ConstructionCostTest {
+@Test
+
+public void testCalculateConstructionCost() {
+ConstructionCost obj1=new ConstructionCost();
+assertEquals("Construction Cost of the house",1140750.0,obj1.CalculateConstructionCost("FullyAutomated",456.3),0);
+}
+}
+
